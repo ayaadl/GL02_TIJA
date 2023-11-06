5c18abb084d9e6ab9d90678850a60e803e60b4e8
ljh321 <ljh1402647237@gmail.com>
2023-11-06 09:18:40 +0100

    Ajout du satellite1,satellite2

---
+++ satellite1.js
@@ -0,0 +1,8 @@
+var satellite1 = {
+	check: function(){
+	console.log("Satellite& Ready!")
+	return true
+	}
+}
+module.exports = satellite1
+
---
+++ satellite2.js
@@ -0,0 +1,9 @@
+
+var satellite2 = {
+        check: function(){
+        console.log("Satellite2 Ready!")
+        return true
+        }
+}
+module.exports = satellite2
+
