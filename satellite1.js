16e4b4767b3d8648817ee9577a9bc012bbb1f7ca
Thomas Martinez <thomas.martinez2001@gmail.com>
2023-11-13 16:32:28 +0900

    magenta added

--- satellite1.js
+++ satellite1.js
@@ -1,6 +1,7 @@
 var satellite1 = {
 	check: function(){
 	console.log("Satellite& Ready!")
+	console.log("Magenta level checked".magenta)
 	return true
 	}
 }
--- satellite2.js
+++ satellite2.js
@@ -2,6 +2,7 @@
 var satellite2 = {
         check: function(){
         console.log("Satellite2 Ready!")
+        console.log("Magenta level checked".magenta)
         return true
         }
 }
